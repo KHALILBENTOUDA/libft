e9bf3c74714dc0797270dd6021c7c1e9723dba9d
Khalil Ben Touda <kben-tou@e2r8p11.1337.ma>
2024-12-08 21:12:41 +0100

    Initial commit of my libft project. Custom implementation of the C standard library for the 42 curriculum.

---
+++ ft_lstlast_bonus.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kben-tou <kben-tou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/31 23:07:49 by kben-tou          #+#    #+#             */
+/*   Updated: 2024/12/08 21:03:16 by kben-tou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	t_list	*last;
+
+	if (!lst)
+		return (NULL);
+	last = lst;
+	while (last->next != NULL)
+		last = last->next;
+	return (last);
+}
